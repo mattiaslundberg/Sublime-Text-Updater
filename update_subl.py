08c074216c9edf45aeabd8580bad320af1ae30ef
Mattias Lundberg <lundberg.mattias@gmail.com>
2013-06-24 20:48:49 +0200

    Support runing updater under python 3.2

--- update_subl.py
+++ update_subl.py
@@ -11,37 +11,45 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
-import urllib, subprocess, os, re
+import urllib, subprocess, os, re, sys
+
+is_py3 = sys.version_info[0] == 3
+if is_py3:
+	import urllib.request as urllib
+else:
+	def input(*args):
+		return raw_input(*args)
 
 version = '2' # Valid choices are '2', '3' and '3dev'
 arch = 32
 
 url = 'http://www.sublimetext.com/%s' % version
-subl_folder = '/opt/'
+subl_folder = '/tmp/'
 
 def _get_latest_urls():
 	sock = urllib.urlopen(url)
-	return re.search("linux[ ,\w,_,-]+%s.*href\=\"(.*?\.tar\.bz2)\""  % arch, sock.read())
+	data = sock.read().decode('utf-8') if is_py3 else sock.read()
+	return re.search("linux[ ,\w,_,-]+%s.*href\=\"(.*?\.tar\.bz2)\""  % arch, data)
 
 def main():
-	url = _get_latest_urls().group(1)
+	download = _get_latest_urls().group(1)
 	
-	assert url != None, 'Download URL not found!'
+	assert download != None, 'Download URL not found!'
 	
-	print( 'Updating to %s' % url )
+	print( 'Updating to %s' % download )
 	
-	filename = latest_url.split('/')[-1]
+	filename = download.split('/')[-1]
 	
-	sock = urllib.urlopen(url)
+	sock = urllib.urlopen(download)
 	data = sock.read()
-	rawfile = file(subl_folder + filename, 'wb')
+	rawfile = open(subl_folder + filename, 'wb')
 	rawfile.write(data)
 	rawfile.close()
 	
 	print( 'Extracting "tar xf %s"' % (subl_folder + filename) )
 	subprocess.Popen(['tar', 'xf', filename], cwd = subl_folder)
 	
-	syml = raw_input("Create symlink in /usr/bin for 'sublime_text' (y/[n]): ")
+	syml = input("Create symlink in /usr/bin for 'sublime_text' (y/[n]): ")
 	
 	if 'y' in syml:
 		print( 'Creating symlink' )
